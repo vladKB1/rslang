07d5805333b1ce892c299ae2205fec2db1f61603
Tsvetinskaya-L <tsvetinskaya.lidiya@gmail.com>
2022-02-18 09:24:59 +0300

    feat: mini-game sprint

---
+++ src/minigame/sprint/sprintController.ts
@@ -0,0 +1,222 @@
+import { SprintView } from './sprintView';
+import { Word, SprintModel } from './sprintModel';
+
+export class SprintController {
+  view: SprintView;
+
+  model: SprintModel;
+
+  container: HTMLElement;
+
+  wrapper: HTMLElement;
+
+  time: number;
+
+  score: number;
+
+  correctTranslation!: string;
+
+  currentWord!: Word;
+
+  wordTranslate!: string;
+
+  currentLevel!: number;
+
+  wrapperCard: HTMLElement;
+
+  correctAnswers: Word[];
+
+  incorrectAnswers: Word[];
+
+  currentPage: number;
+
+  constructor(level = 0, page = 0) {
+    this.currentLevel = level;
+    this.currentPage = page;
+
+    this.view = new SprintView();
+    this.model = new SprintModel();
+    this.container = document.querySelector('.container__mini-game') as HTMLElement;
+    this.wrapper = this.view.createElement('div', 'sprint__field') as HTMLElement;
+    this.wrapperCard = this.view.createElement('div', '.sprint-content__field') as HTMLElement;
+    this.time = 2;
+    this.score = 0;
+    this.correctAnswers = [];
+    this.incorrectAnswers = [];
+  }
+
+  renderHeaderStartPage() {
+    const sprintHeader = this.view.createElement('div', 'sprint__header');
+    sprintHeader.innerHTML = this.view.getHeaderTemplate();
+    this.container.appendChild(sprintHeader);
+  }
+
+  renderMainStartPage() {
+    this.wrapper.innerHTML = this.view.getLevelsNavigationTemplate() + this.view.getButtonsLevelTemplate();
+    this.container.appendChild(this.wrapper);
+  }
+
+  showPageResultSprint() {
+    this.wrapper.innerHTML = '';
+    this.wrapper.innerHTML = this.view.getPageResult(
+      this.correctAnswers.length,
+      this.incorrectAnswers.length,
+      this.score,
+    );
+
+    const correctWordsContainer = document.querySelector('.sprint__result-correct-list');
+    this.correctAnswers.forEach((word: Word) => {
+      const el = document.createElement('div');
+      el.innerHTML = this.view.getResultWordTemplate(word);
+      correctWordsContainer?.append(el);
+    });
+
+    const incorrectWordsContainer = document.querySelector('.sprint__result-incorrect-list');
+    this.incorrectAnswers.forEach((word: Word) => {
+      const el = document.createElement('div');
+      el.innerHTML = this.view.getResultWordTemplate(word);
+      incorrectWordsContainer?.append(el);
+    });
+
+    document.removeEventListener('keyup', this.keyupHandler);
+    this.addPlayWordHander();
+  }
+
+  levelSelection() {
+    const btnSelection = document.querySelector('.level_button_container') as HTMLElement;
+    btnSelection.addEventListener('click', (e: Event) => {
+      this.currentLevel = Number((e.target as HTMLElement).innerHTML) - 1;
+      this.model.getWordsForlevel(this.currentLevel, this.currentPage).then(() => {
+        this.renderBlockTimer();
+        this.renderBlockCard();
+        this.startTimer();
+      });
+    });
+  }
+
+  renderBlockTimer() {
+    this.wrapper.innerHTML = this.view.getCardTimeTemlate();
+    this.container.appendChild(this.wrapper);
+    this.updateScore(0);
+  }
+
+  renderBlockCard() {
+    this.getRandomWord();
+    this.wrapperCard.innerHTML = this.view.getCardWordTemplate(this.currentWord);
+    this.wrapper.appendChild(this.wrapperCard);
+    this.addClickHandler();
+  }
+
+  private getRandomWord(): void {
+    if (this.correctAnswers.length + this.incorrectAnswers.length === this.currentPage * 20 + 20) {
+      this.currentPage++;
+      this.model.getWordsForlevel(this.currentLevel, this.currentPage).then(() => {
+        this.getRandomWord();
+      });
+      return;
+    }
+
+    const word = this.model.words[Math.floor(Math.random() * this.model.words.length)];
+    const word2 = this.model.words[Math.floor(Math.random() * this.model.words.length)];
+
+    const alreadyAnswed = this.correctAnswers.find((element: Word) => {
+      if (element.word === word.word) {
+        return true;
+      }
+    });
+
+    const alreadyAnswed2 = this.incorrectAnswers.find((element: Word) => {
+      if (element.word === word.word) {
+        return true;
+      }
+    });
+    if (alreadyAnswed || alreadyAnswed2) {
+      return this.getRandomWord();
+    }
+    this.correctTranslation = word.wordTranslate;
+    if (Math.random() > 0.5) {
+      word.wordTranslateOnCard = word2.wordTranslate;
+    } else {
+      word.wordTranslateOnCard = word.wordTranslate;
+    }
+    this.currentWord = word;
+  }
+
+  startTimer(): void {
+    const timerNode = document.getElementById('timer');
+    let timerId;
+    if (timerNode) {
+      timerNode.innerText = `${this.time--}`;
+      if (this.time < 0) {
+        clearTimeout(timerId);
+        this.showPageResultSprint();
+      } else {
+        timerId = setTimeout(() => {
+          this.startTimer();
+        }, 1000);
+      }
+    }
+  }
+
+  updateScore(addScore: number) {
+    this.score = this.score + addScore;
+    const scoreNode = document.getElementById('score');
+    if (scoreNode) {
+      scoreNode.innerHTML = `${this.score}`;
+    }
+  }
+
+  private checkAnswer() {}
+
+  addClickHandler() {
+    const btnAnswer = document.querySelector('.sprint-content__field__buttons') as HTMLElement;
+    btnAnswer.addEventListener(
+      'click',
+
+      (event: Event) => {
+        const correctPressed = (event.target as HTMLElement).classList.contains('field__btn__true');
+        const translateCorrect = this.currentWord.wordTranslateOnCard === this.correctTranslation;
+        const answerIsCorrect = (correctPressed && translateCorrect) || (!correctPressed && !translateCorrect);
+        if (answerIsCorrect) {
+          this.updateScore(10);
+          this.correctAnswers.push(this.currentWord);
+        } else {
+          this.incorrectAnswers.push(this.currentWord);
+        }
+        this.renderBlockCard();
+      },
+      { once: true },
+    );
+
+    document.addEventListener('keyup', this.keyupHandler);
+  }
+
+  keyupHandler = (event: KeyboardEvent) => {
+    if (event.code === 'ArrowRight' || event.code === 'ArrowLeft') {
+      const correctPressed = event.code === 'ArrowRight';
+      const translateCorrect = this.currentWord.wordTranslateOnCard === this.correctTranslation;
+      const answerIsCorrect = (correctPressed && translateCorrect) || (!correctPressed && !translateCorrect);
+
+      if (answerIsCorrect) {
+        this.updateScore(10);
+        this.correctAnswers.push(this.currentWord);
+      } else {
+        this.incorrectAnswers.push(this.currentWord);
+      }
+      document.removeEventListener('keyup', this.keyupHandler);
+      this.renderBlockCard();
+    }
+  };
+
+  addPlayWordHander() {
+    const resultContainer = document.querySelector('.sprint__result-page') as HTMLElement;
+    resultContainer?.addEventListener('click', (e: Event) => {
+      const target = e?.target as HTMLElement;
+      if (target.classList.contains('icon-audio')) {
+        const srcAudio = target.getAttribute('data-audio-src');
+        const container = document.querySelector('.audio-container') as HTMLElement;
+        container.innerHTML = this.view.getAudioBlock(`${srcAudio}`);
+      }
+    });
+  }
+}
---
+++ src/minigame/sprint/sprintView.ts
@@ -0,0 +1,102 @@
+import { baseUrl, Word } from './sprintModel';
+
+export class SprintView {
+  createElement(tag: string, className: string) {
+    const element = document.createElement(tag);
+    if (className) element.classList.add(className);
+    return element;
+  }
+
+  getElement(selector: string) {
+    const element = document.querySelector(selector);
+    return element;
+  }
+
+  getHeaderTemplate() {
+    return `
+    <button class="sprint__header__button sprint__sound">
+    <img class="sound-on"></button>
+    <button class="sprint__header__button sprint__home"><img class="home"></button>`;
+  }
+
+  getLevelsNavigationTemplate() {
+    return `
+    <div class="sprint__start-page"
+    <div class="sprint__start-page__main">
+      <h1 class="sprint-title">СПРИНТ</h1>
+      <p class="sprint-description-game">Быстрая игра для проверки Ваших знаний !</p>
+      <p class="sprint-description-game">За 1 минуту Вам нужно выбрать максимальное количество правильных вариантов
+        перевода слова</p>
+      <p class="sprint-description-game">Для начала игры необходимо выбрать уровень сложности, в зависимости от
+        знаний
+  </div>
+  </div>`;
+  }
+
+  getButtonsLevelTemplate() {
+    return `
+    <div class="sprint__start-page__buttons level_button_container">
+        <button class="button-level level-one">1</button>
+        <button class="button-level level-two">2</button>
+        <button class="button-level level-three">3</button>
+        <button class="button-level level-four">4</button>
+        <button class="button-level level-five">5</button>
+        <button class="button-level level-six">6</button>
+      </div>`;
+  }
+
+  getCardTimeTemlate() {
+    return `
+    <div class="sprint-conten">
+    <div class="sprint-conten__header">
+      <div class="sprint-content__clock" id="timer"></div>
+      <div class="sprint-content__counter" id="score"></div>
+    </div>`;
+  }
+
+  getCardWordTemplate(word: Word): string {
+    return `
+    <div class="sprint-content__field">
+        <div class="sprint-content__field__word">${word.word}</div>
+        <div class="sprint-content__field__translate">${word.wordTranslateOnCard}</div>
+        <div class="sprint-content__field__buttons">
+          <button class="sprint-content__field__btn field__btn__false"> Неверно !</button>
+          <button class="sprint-content__field__btn field__btn__true">Верно !</button>
+        </div>
+      </div>
+  </div>`;
+  }
+
+  getPageResult(correctAnswers: number, incorrectAnswers: number, scoreNode: number) {
+    return `
+  <div class="sprint__result-page">
+    <div class="audio-container"></div>
+    <h2 class="sprint__result-title">Твой результат: ${scoreNode} баллов</h2>
+    <div class="sprint__result-list">
+      <h3 class="sprint__result-title-true">Правильных ответов: <span class="false-count">${correctAnswers}</span></h3>
+      <div class="sprint__result-correct-list"></div>
+      <div class="list-separation">
+        <p class="line">-----------------------------------------</p>
+        <h3 class="sprint__result-title-false">Неправильных ответов: <span class="true-count">${incorrectAnswers}</span></h3>
+        <div class="sprint__result-incorrect-list"></div>
+      </div>
+    </div>
+  </div>`;
+  }
+
+  getResultWordTemplate(word: Word) {
+    return `
+    <div class="sprint__result-list__item">
+      <span class="audio-icon__container">
+        <button class="icon-audio" data-audio-src="${baseUrl + word.audio}"><img class="icon-sound">
+        </button>
+        <span class="sprint__result__word">${word.word}</span>
+        <span> - </span>
+        <span class="sprint__result__word-translate">${word.wordTranslate}</span>
+    </div>`;
+  }
+
+  getAudioBlock(src: string): string {
+    return `<audio preload="none" autoplay src="${src}"></audio>`;
+  }
+}
