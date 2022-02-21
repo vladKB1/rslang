461cab61f492edf60a0c72872876edb9bd31fe40
Lidiya-Tsvetinskaya <89584579+Tsvetinskaya-L@users.noreply.github.com>
2022-02-21 15:06:23 +0300

    Feature/minigame sprint (#2)
    * feat: mini-game sprint
    
    * feat:rebased on develop
    
    Co-authored-by: Tsvetinskaya-L <tsvetinskaya.lidiya@gmail.com>

--- src/controllers/MainPageController.ts
+++ src/controllers/MainPageController.ts
@@ -1,3 +1,4 @@
+import { SprintController } from '../minigame/sprint/sprintController';
 import { MainPageModel } from '../models/MainPageModel';
 import { MainPageView } from '../views/MainPageView';
 import { BaseController } from './BaseController';
@@ -5,5 +6,18 @@ import { BaseController } from './BaseController';
 export class MainPageController extends BaseController {
   constructor(model: MainPageModel, view: MainPageView) {
     super(model, view);
+
+    this.addOpenGameHandlers();
   }
+
+  addOpenGameHandlers() {
+    const sprintGame = document.querySelector('.sprint');
+    sprintGame?.addEventListener('click', this.startSprintGame);
+  }
+
+  startSprintGame = () => {
+    const appSprint = new SprintController();
+    appSprint.renderMainStartPage();
+    appSprint.levelSelection();
+  };
 }
